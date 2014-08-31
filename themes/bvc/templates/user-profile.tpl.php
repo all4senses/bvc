0a6c551b4e1954894c582e698800854ae031ee81
U-FLYSMALL\Alex <all4senses@gmail.com>
2014-08-31 22:35:54 +0700

    Submit reviews from Partners

--- themes/bvc/templates/user-profile.tpl.php
+++ themes/bvc/templates/user-profile.tpl.php
@@ -60,13 +60,13 @@
     
     echo '<h1 id="user-caption">Meet' . ($is_reviewer ? ' our reviewer' : '') . ': ' , $user_name, '</h1>', render($user_profile['user_picture']), render($user_profile['field_u_bio']),  '<div class="bottom-clear"></div>';
     
-    if (!$is_reviewer) {
-      $articles = $user_profile['field_u_fname']['#object']->name == 'guest' ? views_embed_view('articles','block_all_by_guest') : views_embed_view('articles','block_all_by_author');
-
-      if ($articles) {
-        echo '<div id="articles-caption">', t('Read some of !author\'s latest articles below:', array('!author' => @$user_profile['field_u_fname'][0]['#markup'])), '</div>', $articles;
-      }
-    }
+//    if (!$is_reviewer) {
+//      $articles = $user_profile['field_u_fname']['#object']->name == 'guest' ? views_embed_view('articles','block_all_by_guest') : views_embed_view('articles','block_all_by_author');
+//
+//      if ($articles) {
+//        echo '<div id="articles-caption">', t('Read some of !author\'s latest articles below:', array('!author' => @$user_profile['field_u_fname'][0]['#markup'])), '</div>', $articles;
+//      }
+//    }
     /*
     $articles = views_embed_view('articles','block_by_author');
     $blog_posts = views_embed_view('blog','block_by_author');
